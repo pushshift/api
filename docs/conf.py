9540ae47d22f03a6cf82d7836d531eccd4014846
root <jason@pushshift.io>
2018-02-11 01:56:02 -0500

    Trying to fix collapsing sidebar

--- docs/conf.py
+++ docs/conf.py
@@ -85,14 +85,13 @@ todo_include_todos = False
 #
 html_theme = 'sphinx_rtd_theme'
 html_style = None
-html_theme_options = {'collapse_navigation': True}
 using_rtd_theme = True
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {'collapse_navigation': True,'navigation_depth': 2}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
